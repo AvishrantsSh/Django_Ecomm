44563dcf6d38e32850a7bd3a71e86206136b40c8
Avishrant Sharma <avishrants@gmail.com>
2020-11-14 14:00:50 +0530

    Login/SignUp Improvements

--- ProjectY/settings.py
+++ ProjectY/settings.py
@@ -135,3 +135,5 @@ LOGOUT_REDIRECT_URL = 'home'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
