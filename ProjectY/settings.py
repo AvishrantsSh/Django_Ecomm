a7001bd24c6ff3e552ccf0f2c1790f6aafff214a
AvishrantsSh <avishrants@gmail.com>
2021-01-08 10:28:31 +0530

    yo

--- ProjectY/settings.py
+++ ProjectY/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'ProjectY.middleware.AuthRequired',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
